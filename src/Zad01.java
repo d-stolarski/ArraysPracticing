c1e38d575ffd4e78e2cf0dfb104b024772c912f0
Daniel Stolarski <d.stolarski@o2.pl>
2017-12-06 09:30:48 +0100

    final commit

---
+++ src/Zad01.java
@@ -0,0 +1,13 @@
+import java.util.Arrays;
+
+public class Zad01 {
+    public static void main(String[] args) {
+
+        String[] names = new String[3];
+        names[0] = "Rafał";
+        names[1] = "Bogdan";
+        names[2] = "Kasia";
+
+        System.out.printf("%s, %s, %s", names[0], names[1], names[2]);
+    }
+}
